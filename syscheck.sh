2ac8d5fc3c566c0bb59585bcf39aa21545d48591
Hms1000 <hmstakai@gmail.com>
2026-06-16 15:27:32 +0200

    Initial commit of the syscheck script

---
+++ syscheck.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e 
+
+check_disk() {
+	FILE_PATH="$1"
+	du -h "$FILE_PATH"
+}
+
+check_users() {
+	who 
+}
+
+check_memory() {
+	free -h 
+}
+
+check_disk "/"
+check_users
+check_memory
