47b3401f34ff933ec3449d7354e27cdc914b1d5c
Hms1000 <hmstakai@gmail.com>
2026-06-17 15:44:55 +0200

    update: created a logfile

--- syscheck.sh
+++ syscheck.sh
@@ -5,7 +5,8 @@ set -e
 check_disk() {
 	FILE_PATH="$1"
 	USAGE=$(df -h "$FILE_PATH" | grep "/$" | awk '{print $5}')
-	echo "Disk usage for $FILE_PATH: $USAGE"
+	TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
+	echo "$TIMESTAMP - Disk usage for $FILE_PATH: $USAGE" >> syscheck.log
 }
 
 check_users() {
