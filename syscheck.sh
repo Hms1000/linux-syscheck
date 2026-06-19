493c247e9d9c08d69beeee959dc70850258c4899
Hms1000 <hmstakai@gmail.com>
2026-06-19 17:13:31 +0200

    upgrade: replaced input redirection with tee to avoid writing unncessary lines of code, tee appends to the log file and prints stdout to the screen at the same time whereas with redirection I had to write extra lines of code to ensure stdout is printed on the screen

--- syscheck.sh
+++ syscheck.sh
@@ -6,9 +6,8 @@ check_disk() {
 	FILE_PATH="$1"
 	USAGE=$(df -h "$FILE_PATH" | grep "/$" | awk '{print $5}')
 	TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
-	echo "$TIMESTAMP - Disk usage for $FILE_PATH: $USAGE" >> syscheck.log
-	echo "$TIMESTAMP - Disk usage for $FILE_PATH: $USAGE"
-	echo " " >> syscheck.log
+	echo "$TIMESTAMP - Disk usage for $FILE_PATH: $USAGE" | tee -a syscheck.log
+	echo " " | tee -a syscheck.log
 	echo " " 
 }
 
@@ -16,22 +15,18 @@ check_users() {
 	USERNAME="$1"
 	LOGGEDUSERS=$(who | grep "^$USERNAME")
 	TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
-	echo "$TIMESTAMP - Logged in sessions for $USERNAME:">> syscheck.log 
-	echo "$LOGGEDUSERS" >> syscheck.log
-	echo "$TIMESTAMP - Logged in sessions for $USERNAME:"
-       	echo "$LOGGEDUSERS"
-	echo " " >> syscheck.log
-        echo " "
+	echo "$TIMESTAMP - Logged in sessions for $USERNAME:" | tee -a syscheck.log
+	echo "$LOGGEDUSERS" | tee -a syscheck.log
+	echo " " | tee -a syscheck.log
+    echo " "
 }
 
 check_memory() {
 	RAM=$(free -h)
 	TIMESTAMP=$(date '+%Y-%m-%d %H:%M:%S')
-	echo "$TIMESTAMP - Memory usage:" >> syscheck.log
-	echo "$RAM" >> syscheck.log
-        echo "$TIMESTAMP - Memory usage:" 
-        echo "$RAM"       	
-	echo " " >> syscheck.log
+	echo "$TIMESTAMP - Memory usage:" | tee -a syscheck.log
+	echo "$RAM" | tee -a syscheck.log
+	echo " " | tee -a syscheck.log
 }
 
 check_disk "/"
